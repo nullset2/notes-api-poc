2936d1b7421c0196dd5bfc90e7c03d38080e1ad2
alfredo <jalfredo.gallegos@gmail.com>
2017-05-28 14:00:34 -0500

    corrects scope name to avoid conflict with AR

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
-  scope :public, where(shared: true)
+  scope :available_publicly, -> { where(shared: true) }
   validates :title, presence: true, length: { maximum: 140 }
   validates :content, presence: true
 
