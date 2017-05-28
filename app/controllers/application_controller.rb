49c3662c77b2152ed1b6743ed4dae8cd32d8fe4a
alfredo <jalfredo.gallegos@gmail.com>
2017-05-28 12:33:06 -0500

    adds response on root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def welcome 
+    render plain: "Note app API --please sign in"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :notes
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "application#welcome"
 end
