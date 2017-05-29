462210175148bd5217a30eb96a7917beef6b96f8
alfredo <jalfredo.gallegos@gmail.com>
2017-05-28 22:52:43 -0500

    fix model names

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,6 @@
 class Note < ApplicationRecord
   scope :publicly_available, -> { where(shared: true) }
   validates :title, presence: true, length: { maximum: 140 }
-  validates :content, presence: true
 
   belongs_to :user
   has_many :attachments, dependent: :destroy
